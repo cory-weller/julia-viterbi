604033f642118a5a979ff36cc76513b642264b93
itsyosef <jho5ze@virginia.edu>
2019-04-16 17:43:04 -0400

    Updated viterbi to correctly handle indexing - now we just need to work on transition and emission values

--- example/Joseph-Viterbi-First-Implementation.jl
+++ example/Joseph-Viterbi-First-Implementation.jl
@@ -2,12 +2,11 @@
 #medium 1/10 of number of observations should be about 10min.
 #Shuffle the file to get some subset of the
 #Need to parse info file talking about how position can changetransition values - file incorporates function of recombination rate at different nucleotide distances (centimorgans) to see how likely recombination between.
-#tell it how recombinant the inidv are to get scalar to mult by the recombination rate - use the viterbi 
+#tell it how recombinant the inidv are to get scalar to mult by the recombination rate - use the viterbi
 
 function get_emission_value(observation, state_index)
 
     global HAPS_LINE
-    #println("Heres a HAPS_LINE in emission: ", HAPS_LINE)
     if state_index%num_haplotypes == 0
         second_value = HAPS_LINE[num_haplotypes + to_add_to_first_haplotype]
     else
@@ -15,45 +14,41 @@ function get_emission_value(observation, state_index)
     end
 
     state_tuple = (HAPS_LINE[cld(state_index,(num_haplotypes)) + to_add_to_first_haplotype], second_value)
-    #println("Emission State Tuple")
-    #println(string(state_index, " : ", state_tuple))
-    return floor(Int, state_index)
+
+    return floor(Int, parse(Int,state_tuple[1]) + parse(Int, state_tuple[2]))
 
 end
 
 function get_transition_value(previous_state_index, state_index)
 
-    global HAPS_LINE
-    global prev_HAPS_LINE
+    global header_names
 
     if state_index%num_haplotypes == 0
-        cur_second_value = HAPS_LINE[num_haplotypes + to_add_to_first_haplotype]
+        cur_second_value = header_names[num_haplotypes + to_add_to_first_haplotype]
     else
-        cur_second_value = HAPS_LINE[state_index%(num_haplotypes) + to_add_to_first_haplotype]
+        cur_second_value = header_names[state_index%(num_haplotypes) + to_add_to_first_haplotype]
     end
 
     if previous_state_index%num_haplotypes == 0
-        prev_second_value = prev_HAPS_LINE[num_haplotypes + to_add_to_first_haplotype]
+        prev_second_value = header_names[num_haplotypes + to_add_to_first_haplotype]
     else
-        prev_second_value = prev_HAPS_LINE[previous_state_index%(num_haplotypes) + to_add_to_first_haplotype]
+        prev_second_value = header_names[previous_state_index%(num_haplotypes) + to_add_to_first_haplotype]
     end
 
-    state_tuple = (HAPS_LINE[cld(state_index,(num_haplotypes)) + to_add_to_first_haplotype], cur_second_value)
+    state_tuple = (header_names[cld(state_index,(num_haplotypes)) + to_add_to_first_haplotype], cur_second_value)
 
-    previous_state_tuple = (prev_HAPS_LINE[cld(previous_state_index,(num_haplotypes)) + to_add_to_first_haplotype], prev_second_value)
-    #println("prev_HAPS_LINE: ", prev_HAPS_LINE)
-    #println("HAPS_LINE : ", HAPS_LINE)
-    #print("Transition State Tuple ")
-    #print(string(state_index, " : ", state_tuple, " , "))
-    #println(string(previous_state_index, " : ", previous_state_tuple, " end "))
+    previous_state_tuple = (header_names[cld(previous_state_index,(num_haplotypes)) + to_add_to_first_haplotype], prev_second_value)
 
-    return floor(Int, previous_state_index)
+    if previous_state_index == state_index
+        return 1
+    else
+        return 0
+    end
 end
 
-#Need to make the prev_HAPS_LINE and update both of them during the algorithm
 first_time = time()
-observation_file = open("observations.dat") #opening the files
-haplotypes_file = open("test.dat") #opening the files
+observation_file = open(ARGS[1]) #opening the files
+haplotypes_file = open(ARGS[2]) #opening the files
 
 observations = readlines(observation_file) #reading in the lines - May be too big to deal with
 haplotypes = readlines(haplotypes_file) #reading in the readlines
@@ -61,7 +56,6 @@ OBS_ALLELE = (split(observations[2]))[2] #the observation being dealt with
 
 global to_add_to_first_haplotype = 4 #index of first haplotype info minus one (offset)
 global HAPS_LINE = split(haplotypes[2]) #holds the current row to deal with
-global prev_HAPS_LINE = split(haplotypes[2]) #holds the previous row to deal with
 global num_haplotypes = length(HAPS_LINE) - to_add_to_first_haplotype #holds the number of haplotypes
 
 start_of_observations_row = 2 #first row with data
@@ -81,12 +75,8 @@ end
 print("[")
 
 #findmax(a[:,5])
-#(15, 3)
 
 for observation_number in (start_of_observations_row + 1):(num_SNPs + 1)
-
-    #println("num_SNPs: ", num_SNPs)
-    #println("current row: ", observation_number)
 
     #if observation_number % floor(Int, num_SNPs/1) == 0
         print(".")
@@ -96,9 +86,6 @@ for observation_number in (start_of_observations_row + 1):(num_SNPs + 1)
     global prev_HAPS_LINE = split(haplotypes[observation_number - 1])
     global HAPS_LINE = split(haplotypes[observation_number])
 
-    #println("Updated prev_HAPS_LINE: ", prev_HAPS_LINE)
-    #println("Updated HAPS_LINE: ", HAPS_LINE)
-
     for haplotype_state in 1:(num_haplotypes^2) #OPTIMIZATION NEEDED
         finding_the_way = Array{Float64}(undef, (num_haplotypes)^2)
         for previous_state in 1:(num_haplotypes^2) #OPTIMIZATION NEEDED
@@ -107,7 +94,6 @@ for observation_number in (start_of_observations_row + 1):(num_SNPs + 1)
 
         end
 
-        #println("finding the way outside: ", finding_the_way)
         maxval, maxpos = findmax(finding_the_way)
         most_likely_previous_paths[haplotype_state, observation_number - 1] = floor(Int, maxpos)
         most_likely_paths_so_far[haplotype_state, observation_number - 1] = maxval*get_emission_value(OBS_ALLELE, haplotype_state)
@@ -118,21 +104,28 @@ end
 
 println("]")
 
-#println("most_likely_previous_paths: ", most_likely_previous_paths)
-#println("most_likely_paths_so_far: ", most_likely_paths_so_far)
-
 prob_max_path, final_hap = findmax(most_likely_paths_so_far[:,num_SNPs])
-final_path = Array{Int64}(undef, num_SNPs)
+final_path = Array{String}(undef, num_SNPs)
 
 back_trace = final_hap
-final_path[num_SNPs] = back_trace
+back_trace_second = back_trace%num_haplotypes
+if back_trace_second == 0
+    back_trace_second = num_haplotypes
+end
+final_path[num_SNPs] = string(header_names[cld(back_trace,num_haplotypes) + to_add_to_first_haplotype] , " : ", header_names[back_trace_second + to_add_to_first_haplotype])
 for back_trace_step in 1:(num_SNPs - 1)
-    #println("back_trace: ", back_trace)
     global back_trace = floor(Int, most_likely_previous_paths[back_trace, num_SNPs - back_trace_step + 1])
-    final_path[num_SNPs - back_trace_step] = back_trace
+
+    back_trace_second = back_trace%num_haplotypes
+
+    if back_trace_second == 0
+        back_trace_second = num_haplotypes
+    end
+    final_path[num_SNPs - back_trace_step] = string(header_names[cld(back_trace,num_haplotypes) + to_add_to_first_haplotype], " : " , header_names[back_trace_second + to_add_to_first_haplotype])
 
 end
 
-#println("The end: ", final_path)
+println("The end: ", final_path)
 second_time = time()
 println("time elapsed = ", (second_time - first_time))
+println("max prob: ", prob_max_path)
