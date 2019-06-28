0737d9f47795ec7319b65af2fbc6012261ded41a
itsyosef <jho5ze@virginia.edu>
2019-06-28 17:02:55 -0400

    Fixed up the viterbi portion to test on Rivanna.

--- example/Viterbi-First-Implementation.jl
+++ example/Viterbi-First-Implementation.jl
@@ -1,3 +1,4 @@
+#!/usr/bin/env julia
 #Joseph Outten - viterbi algorithm implementation
 #medium 1/10 of number of observations should be about 10min.
 #Shuffle the file to get some subset of the
@@ -9,11 +10,14 @@
 #Get some parallelization
 #PASS THESE IN INSTEAD OF A BUNCH OF GLOBAL VARIABLES
 
+#MAY NEED TO MAKE IT A BUNCH OF FUNCTIONS INSTEAD TO REALLY SPEED UP RUNTIME AND AVOID GLOBAL VARIABLES
+
 using DelimitedFiles
 using LinearAlgebra
-using ProgressBars
 
 function get_emission_value(observation, typed_prediction, IBD, f_error, t_error) #ToDo CLean this up with maybe passing in an array that can be called for the different cases to make it more readable and concise and possibly faster...
+
+    # println("Observation: ", observation)
 
     if observation == "0"
 
@@ -195,20 +199,21 @@ function get_emission_value(observation, typed_prediction, IBD, f_error, t_error
 
         else
             print("Wrong haplotype file format.")
+            return 0
 
         end
 
     else
         print("Wrong observation file format")
-        return
+        return 0
 
     end
 
-    #   return 0.9
+    # return 0.9
 
 end
 
-function get_transition_value(previous_state_index, state_index)
+function get_transition_value(previous_state_index, state_index,header_names)
 
     #take the final column and multiply by 0.001 to get the probability of the
     #recombination in that range, then take the cumulative sum of the probabilities in
@@ -223,13 +228,14 @@ function get_transition_value(previous_state_index, state_index)
 
     #This may need to be updated to reflect that there can be two crossover events in one location
 
-    global header_names
 
     # if state_index%num_haplotypes == 0
     #     cur_second_value = header_names[num_haplotypes + index_offset_of_first_haplotype]
     # else
     #     cur_second_value = header_names[state_index%(num_haplotypes) + index_offset_of_first_haplotype]
     # end
+
+    # global header_names
 
     cur_second_value = header_names[(state_index - 1)%(num_haplotypes) + index_offset_of_first_haplotype + 1] #need to do "+1 -1" to ensure that the last item (mod of it == 0) is mapped to the last haplotype combo...Julia array starting at 1 kinda necessitates this
 
@@ -245,9 +251,9 @@ function get_transition_value(previous_state_index, state_index)
     previous_state_tuple = (header_names[cld(previous_state_index,(num_haplotypes)) + index_offset_of_first_haplotype], prev_second_value)
 
     if previous_state_index == state_index
-        return 0.99
+        return 0.999
     elseif (previous_state_tuple[1] == state_tuple[1]) || (previous_state_tuple[2] == state_tuple[2])
-        return 0.01
+        return 0.001
     else
         return 0
     end
@@ -258,10 +264,14 @@ first_time = time()
 observation_file = open(ARGS[1]) #opening the files
 haplotypes_file = open(ARGS[2]) #opening the files
 
-observations = readdlm(observation_file) #reading in the lines - May be too big to deal with
-positions = observations[:,1]
+observations = readdlm(observation_file, String) #reading in the lines - May be too big to deal with
+test_observations = readdlm(open("alt_observations.dat"), Char)
+# println("Beginning of the test file: ", test_observations[1:10])
+num_rows = length(observations[:,1])
+# positions = observations[:,1]
 observations = observations[:,2]
-haplotypes = readdlm(haplotypes_file) #reading in the readlines
+# observations = test_observations
+haplotypes = readdlm(haplotypes_file, String) #reading in the readlines
 OBS_ALLELE = observations[2] #the observation being dealt with
 close(observation_file)
 num_SNPs = min(length(observations), length(haplotypes[:,1])) - 1
@@ -283,7 +293,7 @@ emission_value_dict = Dict{String,Float64}() #will store the translation from ob
 
 most_likely_paths_so_far = Array{Float64}(undef, num_hap_combinations, num_SNPs) #holds the probability of the most likely path so far to this haplotype
 most_likely_previous_paths = Array{Float64}(undef, num_hap_combinations, num_SNPs) #holds where the most likely path thus far came from
-
+#
 for initial_prob_position in 1:(num_haplotypes^2)
 
     founder_1 = HAPS_LINE[cld(initial_prob_position, (num_haplotypes)) + index_offset_of_first_haplotype]
@@ -294,7 +304,7 @@ for initial_prob_position in 1:(num_haplotypes^2)
         IBD = 0
     end
     # println("log of the emission: ",  log(100, get_emission_value(string(OBS_ALLELE), string(founder_1, founder_2), IBD, founder_allelic_error, offspring_allelic_error)))
-    most_likely_paths_so_far[initial_prob_position,1] = 1 * log(get_emission_value(string(OBS_ALLELE), string(founder_1, founder_2), IBD, founder_allelic_error, offspring_allelic_error))
+    most_likely_paths_so_far[initial_prob_position,1] = 1 * log(get_emission_value(OBS_ALLELE, founder_1*founder_2, IBD, founder_allelic_error, offspring_allelic_error))
     most_likely_previous_paths[initial_prob_position,1] = 0
 
 end
@@ -302,22 +312,31 @@ end
 # print("[")
 
 #findmax(a[:,5])
+to_mod = ceil(Int, num_rows/1000)
+counter = 0.0
 
 for observation_number in (start_of_observations_row + 1):(num_SNPs + 1)
-
+    # print("num___rows: ", num_rows)
+    if (observation_number % to_mod == 0)
+        global counter += 1.0
+        print("The code is ", counter/10, "% done. ", '\r')
+    end
     # if observation_number % 50 == 0
     #     print(".")
     # end
 
     OBS_ALLELE = observations[observation_number]
-    global prev_HAPS_LINE = haplotypes[observation_number - 1,:]
-    global HAPS_LINE = haplotypes[observation_number,:]
+    # global
+    prev_HAPS_LINE = haplotypes[observation_number - 1,:]
+    # global
+    HAPS_LINE = haplotypes[observation_number,:]
 
     for haplotype_state in 1:(num_haplotypes^2) #OPTIMIZATION NEEDED
         finding_the_way = Array{Float64}(undef, num_hap_combinations)
         for previous_state in 1:(num_haplotypes^2) #OPTIMIZATION NEEDED
 
-            finding_the_way[previous_state] = most_likely_paths_so_far[previous_state,observation_number - 2]+log(  get_transition_value(previous_state,haplotype_state)) #observation_number - 2 because you have -1 since obs starts at 2 and also -1 since we're checking the previous value
+            finding_the_way[previous_state] = most_likely_paths_so_far[previous_state,observation_number - 2]+log(get_transition_value(previous_state,haplotype_state, header_names)) #observation_number - 2 because you have -1 since obs starts at 2 and also -1 since we're checking the previous value
+            # @time get_transition_value(previous_state, haplotype_state)
 
         end
         maxval, maxpos = findmax(finding_the_way)
@@ -331,7 +350,8 @@ for observation_number in (start_of_observations_row + 1):(num_SNPs + 1)
         else
             IBD = 0
         end
-        most_likely_paths_so_far[haplotype_state, observation_number - 1] = maxval+log(  get_emission_value(string(OBS_ALLELE), string(founder_1,founder_2), IBD, founder_allelic_error, offspring_allelic_error))
+        most_likely_paths_so_far[haplotype_state, observation_number - 1] = maxval+log(get_emission_value(OBS_ALLELE, founder_1*founder_2, IBD, founder_allelic_error, offspring_allelic_error))
+        # @time get_emission_value(Int,OBS_ALLELE, string(founder_1*founder_2), IBD, founder_allelic_error, offspring_allelic_error)
 
     end
 
@@ -361,11 +381,11 @@ for back_trace_step in 1:(num_SNPs - 1)
 end
 
 second_time = time()
-print("")
+println("")
 println("time elapsed = ", (second_time - first_time))
 #println("The end: ", final_path)
 haplotypes = haplotypes[:,2]
-println(haplotypes[2], " : " , final_path[1])
+# println(haplotypes[2], " : " , final_path[1])
 output = Array{String}[]
 push!(output, split(final_path[1], " : "))
 push!(output[1], "1")
@@ -382,7 +402,7 @@ while indexing < length(final_path)
     # if indexing >= length(final_path)
     #     break
     # end
-    println(haplotypes[indexing+1], " : ", final_path[indexing])
+    # println(haplotypes[indexing+1], " : ", final_path[indexing])
     push!(output, split(final_path[indexing], " : "))
     push!(output[output_indexing], string(haplotypes[indexing+1]))
     global output_indexing += 1
@@ -392,22 +412,25 @@ end
 # println("time elapsed = ", (second_time - first_time))
 println("max prob: ", prob_max_path)
 
-println("haplotype", ' ', "start", '\t', "stop", '\t', "ind", '\t', "sex", '\t', "lineID")
+println("haplotype", '\t', "start", '\t', "stop", '\t', "ind", '\t', "sex", '\t', "lineID")
 prev_entry = ""
 line = ""
 for haplotype in 1:2
-    global line = string(haplotype, "  ", '\t', "1", '\t')
-    global prev_entry = output[1][haplotype]
+
+    # global
+    line = string(haplotype, '\t', '\t',"1", '\t')
+    # global
+    prev_entry = output[1][haplotype]
     for row_num in 2:length(output)
+        # global output
         entry = output[row_num][haplotype]
         if entry != prev_entry
-            global line = line * string(output[row_num][3], '\t', "-", '\t', "-", prev_entry)
+            line = line * string(output[row_num][3], "   ", '\t', "-", '\t', "-", '\t', prev_entry)
             println(line)
-            global line = string(haplotype, "  ",  '\t', output[row_num][3], '\t')
-            global prev_entry = entry
+            line = string(haplotype, "  ",  '\t', "   " ,output[row_num][3], '\t')
+            prev_entry = entry
         end
     end
+    line = line * string(output[length(output)][3], "   ", '\t', "-", '\t', "-", '\t', prev_entry)
+    println(line)
 end
-
-line = line * string(output[length(output)][1], '\t', "-", '\t', "-", '\t', prev_entry)
-println(line)
